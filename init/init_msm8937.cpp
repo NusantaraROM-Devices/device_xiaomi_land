3b3eabee7c5a09e7a64d8f7d4d50dcb0e5d674ce
Albert I <kras@raphielgang.org>
2020-07-26 22:04:12 +0800

    land: Init Go configurations
    * Set all Go properties for 2 GB RAM, LMK only for 3 GB RAM
    * Import stripped Go configuration intended to only improve RAM usage, which
      applied globally unfortunately
    * This should least affecting 4 GB RAM
    
    * Adapted for land
    
    Signed-off-by: Albert I <kras@raphielgang.org>
    Signed-off-by: Aoihara <veronicadisa6@gmail.com>
    Change-Id: I2192e6fa770b1823752a8a05e8e6841b9ce331a0

--- init/init_msm8937.cpp
+++ init/init_msm8937.cpp
@@ -64,29 +64,43 @@ void check_device()
 
     sysinfo(&sys);
 
-    if (sys.totalram > 3072ull * 1024 * 1024) {
-        // from - phone-xxhdpi-4096-dalvik-heap.mk
-        heapstartsize = "16m";
-        heapgrowthlimit = "256m";
-        heapsize = "512m";
-        heapminfree = "4m";
-        heapmaxfree = "8m";
-    } else if (sys.totalram > 2048ull * 1024 * 1024) {
-        // from - phone-xxhdpi-3072-dalvik-heap.mk
-        heapstartsize = "16m";
-        heapgrowthlimit = "128m";
-        heapsize = "768m";
-        heapminfree = "4m";
-	heapmaxfree = "8m";
-    } else {
-        // from - phone-xxhdpi-2048-dalvik-heap.mk
-        heapstartsize = "16m";
+    // set different Davlik heap properties for 2 GB
+    if (sys.totalram > 2048ull * 1024 * 1024) {
         heapgrowthlimit = "192m";
         heapsize = "512m";
-        heapminfree = "2m";
+        // from phone-xhdpi-4096-dalvik-heap.mk
+        heaptargetutilization = "0.6";
+        heapminfree = "8m";
+        heapmaxfree = "16m";
+    } else {
+        // from go_defaults_common.prop
+        heapgrowthlimit = "128m";
+        heapsize = "256m";
+        // from phone-xhdpi-2048-dalvik-heap.mk
+        heaptargetutilization = "0.75";
+        heapminfree = "512k";
         heapmaxfree = "8m";
+}
 
-   }
+    // set Go tweaks for LMK for 2/3 GB
+    if (sys.totalram < 3072ull * 1024 * 1024) {
+        property_set("ro.lmk.critical_upgrade", "true");
+        property_set("ro.lmk.upgrade_pressure", "40");
+        property_set("ro.lmk.downgrade_pressure", "60");
+        property_set("ro.lmk.kill_heaviest_task", "false");
+    }
+
+    // set rest of Go tweaks for 2 GB
+    if (sys.totalram < 2048ull * 1024 * 1024) {
+        // set lowram options and enable traced by default
+        property_set("ro.config.low_ram", "true");
+        property_set("persist.traced.enable", "true");
+        property_set("ro.statsd.enable", "true");
+        // set threshold to filter unused apps
+        property_set("pm.dexopt.downgrade_after_inactive_days", "10");
+        // set the compiler filter for shared apks to quicken
+        property_set("pm.dexopt.shared", "quicken");
+    }
 }
 
 void set_zram_size(void)
@@ -122,7 +136,7 @@ void vendor_load_properties()
     property_set("dalvik.vm.heapstartsize", heapstartsize);
     property_set("dalvik.vm.heapgrowthlimit", heapgrowthlimit);
     property_set("dalvik.vm.heapsize", heapsize);
-    property_set("dalvik.vm.heaptargetutilization", "0.75");
+    property_set("dalvik.vm.heaptargetutilization", heaptargetutilization);
     property_set("dalvik.vm.heapminfree", heapminfree);
     property_set("dalvik.vm.heapmaxfree", heapmaxfree);
 
