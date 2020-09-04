b634613518510baaf32e5574f0ff5d37b659860f
Aoihara <veronicadisa6@gmail.com>
2020-09-05 06:13:21 +0700

    Revert "land: Add MIUI Poweroff Charging Animation"
    *it doesn't work kek
    
    This reverts commit f3373ffeac307b2890eee907a040d2a9ff2a574c.

--- device.mk
+++ device.mk
@@ -129,9 +129,6 @@ PRODUCT_PACKAGES += \
     android.hardware.ir@1.0-impl \
     android.hardware.ir@1.0-service
 
-# Charger
-include $(LOCAL_PATH)/rootdir/charger/charger.mk
-
 # Display
 PRODUCT_PACKAGES += \
     android.hardware.graphics.allocator@2.0-impl \
