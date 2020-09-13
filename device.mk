316f51a736b63456cdcb3df0fcf4f1a3d7c2641a
Aoihara <veronicadisa6@gmail.com>
2020-09-13 10:09:44 +0000

    land: Build Camera2

--- device.mk
+++ device.mk
@@ -134,7 +134,7 @@ PRODUCT_PACKAGES += \
     android.hardware.camera.provider@2.4-impl \
     android.hardware.camera.provider@2.4-service \
     camera.msm8937 \
-    Snap
+    Camera2
 
 # ConsumerIr
 PRODUCT_PACKAGES += \
