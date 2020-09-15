6ad3517d232ba413e3faac9f68a6f0ac5d2e71d3
Aoihara <veronicadisa6@gmail.com>
2020-09-15 21:13:01 +0700

    land: Add hals namespaces
    Signed-off-by: Aoihara <veronicadisa6@gmail.com>

--- device.mk
+++ device.mk
@@ -33,6 +33,12 @@ TARGET_SCREEN_WIDTH := 1080
 PRODUCT_SOONG_NAMESPACES += \
     $(LOCAL_PATH)
 
+# Hals namespaces
+PRODUCT_SOONG_NAMESPACES += \
+    hardware/qcom-caf/msm8996/display \
+    hardware/qcom-caf/msm8996/audio \
+    hardware/qcom-caf/msm8996/media
+
 # Permissions
 PRODUCT_COPY_FILES += \
     external/ant-wireless/antradio-library/com.dsi.ant.antradio_library.xml:$(TARGET_COPY_OUT_VENDOR)/etc/permissions/com.dsi.ant.antradio_library.xml \
