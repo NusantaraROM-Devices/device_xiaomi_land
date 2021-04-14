sairam1411/android_device_xiaomi_land

land: init: Enable config_avoidGfxAccel for 2GB variants
- This flag turns off GPU/CPU intensive UI effects and
  reduces the RAM footprint.

- Using this is a better compromise than the 'low_ram' flag
  as this does not disable features in the rom but still
  reduces RAM usage and gives better smoothness in the UI.

Change-Id: I7fc645e5089e3c06c16ec8ac73b0fec5840cefb4
Co-authored-by: Jyotiraditya <jyotiraditya@aospa.co>

init/init_msm8937.cpp
@@ -97,10 +97,20 @@ void check_device()
    }
 }
 
+void set_avoid_gfxaccel_config() {
+    struct sysinfo sys;
+    sysinfo(&sys);
+
+    if (sys.totalram <= 2048ull * 1024 * 1024) {
+        // Reduce memory footprint
+        property_override("ro.config.avoid_gfx_accel", "true");
+    }
+}
 
 void vendor_load_properties()
 {
     check_device();
+    set_avoid_gfxaccel_config();
 
     property_override("dalvik.vm.heapstartsize", heapstartsize);
     property_override("dalvik.vm.heapgrowthlimit", heapgrowthlimit);

NusantaraROM-Devices/device_xiaomi_land

Signed-off-by: Aoihara <veronicadisa6@gmail.com>

init/init_msm8937.cpp
@@ -88,6 +88,16 @@ void check_device()
    }
 }
 
+void set_avoid_gfxaccel_config() {
+    struct sysinfo sys;
+    sysinfo(&sys);
+
+    if (sys.totalram <= 2048ull * 1024 * 1024) {
+        // Reduce memory footprint
+        property_set("ro.config.avoid_gfx_accel", "true");
+    }
+}
+
 void set_zram_size(void)
 {
     FILE *f = fopen("/sys/block/zram0/disksize", "wb");
@@ -117,6 +127,7 @@ void vendor_load_properties()
 {
     check_device();
     set_zram_size();
+    set_avoid_gfxaccel_config();
 
     property_set("dalvik.vm.heapstartsize", heapstartsize);
     property_set("dalvik.vm.heapgrowthlimit", heapgrowthlimit);